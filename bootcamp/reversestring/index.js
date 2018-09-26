15350f006df628479e4368bf775f47cc5a505da8
Jamie Pham <jtkp66@gmail.com>
2018-09-26 12:44:45 -0700

    Added code from Grider course

---
+++ bootcamp/reversestring/index.js
@@ -0,0 +1,37 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// function reverse(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//     return arr.join('');
+// }
+
+// function reverse(str) {
+//     var reversed = '';
+    
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
+// function reverse(str) {
+//    return str.split('').reduce((reversed, character) => {
+//         return character
+//     }, '');
+// }
+
+// function reverse(str) {
+//     str.split('').reduce((rev, char) => char + rev, '');
+// }
+
+module.exports = reverse;
